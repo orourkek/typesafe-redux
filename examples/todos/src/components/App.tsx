0dd6e54ddeae8b334116cc12b65995eb145ed9c1
Kevin O'Rourke <korourke@oblong.com>
2019-03-11 14:16:24 -0700

    example app: move provider to index.tsx, remove service worker

--- examples/todos/src/components/App.tsx
+++ examples/todos/src/components/App.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
-import { Provider } from 'react-redux';
 import AddTodoContainer from '../containers/AddTodoContainer';
 import TodoListContainer from '../containers/TodoListContainer';
-import store from '../store';
 
 const App = () => (
-  <Provider store={ store }>
+  <div>
     <AddTodoContainer />
     <TodoListContainer />
-  </Provider>
+  </div>
 );
 
 export default App;
